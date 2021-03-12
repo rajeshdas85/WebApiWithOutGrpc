d19984acf9551d404db994ea4c8f79b47560f0ad
rajesh das <rajesh.das@evolutyz.com>
2021-03-12 12:36:20 +0530

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiWithOutGrpc.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<User> GetUsers()
+        {
+            return Enumerable.Range(1, 10000).Select(x => new User
+            {
+                Id= x,
+                Name = $"Name {x} from Sales"
+
+            }).ToArray();
+        }
+    }
+}
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
